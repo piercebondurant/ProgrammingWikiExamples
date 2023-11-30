996f1275c6e1a6f712c4352f41184b2a837f8cbd
piercebondurant <pierce.bondurant@gmail.com>
2023-11-30 00:26:38 -0600

    PrimeOneToFifty init + breakdown

---
+++ ProgrammingWikiExamples/Exercises/Loops/PrimeOneToFifty/PrimeOneToFifty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingWikiExamples.Exercises.Loops.PrimeOneToFifty
+{
+    public class PrimeOneToFifty
+    {
+        //write a function that prints all prime numbers between 1 to 50 to the console window.
+    }
+}
