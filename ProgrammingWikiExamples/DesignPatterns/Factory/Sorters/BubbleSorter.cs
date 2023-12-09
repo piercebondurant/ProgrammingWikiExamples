8ab5b2137ef39909ea49d8afb0e75251a0198d68
Leo Loureiro <leolou.90@gmail.com>
2023-12-09 16:08:07 -0700

    Add Bubble Sort Algoritm

--- ProgrammingWikiExamples/DesignPatterns/Factory/Sorters/BubbleSorter.cs
+++ ProgrammingWikiExamples/DesignPatterns/Factory/Sorters/BubbleSorter.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ProgrammingWikiExamples.DesignPatterns.Factory.Sorters
 {
-    internal class BubbleSorter
+    public class BubbleSorter
     {
+        public int[] Sort(int[] array)
+        {
+            for(int i  = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                        
+                    }
+                }
+            }
+
+            return array;
+        }
+        
     }
 }
